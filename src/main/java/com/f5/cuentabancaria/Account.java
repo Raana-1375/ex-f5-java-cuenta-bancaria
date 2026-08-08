a10de5a36f0fd20b87d4867a599f27aa9aeb5313
Raana <raanaafkari96@gmail.com>
2026-08-08 10:46:36 +0200

    feat: add Account base class with protected attributes

---
+++ src/main/java/com/f5/cuentabancaria/Account.java
@@ -0,0 +1,11 @@
+package com.f5.cuentabancaria;
+
+public class Account {
+
+    protected float balance;
+    protected int depositCount;
+    protected int withdrawalCount;
+    protected float annualInterestRate;
+    protected float monthlyFee;
+
+}
